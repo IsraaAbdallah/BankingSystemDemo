8920dab8eea5e8699acec6b1ec403bff22b23371
IsraaAbdallah <esraaadallah392@gmail.com>
2023-07-28 16:31:01 +0300

    Commiting source file

---
+++ DepositTransaction.java
@@ -0,0 +1,12 @@
+public class DepositTransaction extends Transaction{
+    float amount;
+    public DepositTransaction(float amount) {
+        this.amount = amount;
+    }
+    @Override
+    void performTransaction(BankAccount bankAccount) {
+        super.performTransaction(bankAccount);
+        bankAccount.withdraw(amount);
+
+    }
+}
